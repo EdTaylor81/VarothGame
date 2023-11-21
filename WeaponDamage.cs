256b151a2cfc7ba565567cec77be4528bcdba4ec
EdTaylor81 <151592846+EdTaylor81@users.noreply.github.com>
2023-11-20 20:08:12 -0600

    Add files via upload

---
+++ WeaponDamage.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDamage : MonoBehaviour
+{
+    [SerializeField] private Collider myCollider;
+
+    private int damage;
+
+    private List<Collider> alreadyCollidedWith = new List<Collider>();
+
+    private void OnEnable()
+    {
+        alreadyCollidedWith.Clear();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == myCollider) { return; }
+
+        if (alreadyCollidedWith.Contains(other)) { return; }
+
+        alreadyCollidedWith.Add(other);
+
+        if (other.TryGetComponent<Health>(out Health health))
+
+        {
+            health.DealDamage(damage);
+        }
+    }
+
+    internal void SetAttack(float damage)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetAttack(int damage)
+    {
+        this.damage = damage;
+    }
+
+}
